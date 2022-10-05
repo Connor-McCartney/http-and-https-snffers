54a6636d3a05b5743c46fb08fba6a8eaec2d34c8
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-10-05 16:31:22 +1000

    Create http-sniffer.py

---
+++ http-sniffer.py
@@ -0,0 +1,17 @@
+from scapy.all import sniff
+from scapy.layers.http import HTTPRequest, Raw
+
+def http_sniff(packet):
+    if packet.haslayer(HTTPRequest):
+        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
+        print(url)
+        if packet.haslayer(Raw):
+            data = packet[Raw].load.decode().split("&")
+            print(data)
+
+def main():
+    print("HTTP sniffing started...")
+    sniff(filter="port 80", prn=http_sniff)
+
+if __name__ == "__main__":
+    main()
