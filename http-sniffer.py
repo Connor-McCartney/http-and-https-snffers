3bfa2f256dceea6f67016d7be648886902582042
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-11-23 21:39:58 +1000

    Update http-sniffer.py

--- http-sniffer.py
+++ http-sniffer.py
@@ -1,17 +1,20 @@
 from scapy.all import sniff
 from scapy.layers.http import HTTPRequest, Raw
 
+
 def http_sniff(packet):
     if packet.haslayer(HTTPRequest):
         url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
         print(url)
         if packet.haslayer(Raw):
-            data = packet[Raw]
+            data = packet[Raw].load.decode()
             print(data)
 
+            
 def main():
     print("HTTP sniffing started...")
     sniff(filter="port 80", prn=http_sniff)
 
+    
 if __name__ == "__main__":
     main()
